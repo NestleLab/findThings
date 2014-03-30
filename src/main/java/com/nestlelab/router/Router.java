66bcd1649b27f0b3c1590246b41292da7cf50779
cynricshu <cynricshu@gmail.com>
2014-03-30 15:24:23 +0800

    build the project structure

---
+++ src/main/java/com/nestlelab/router/Router.java
@@ -0,0 +1,28 @@
+package com.nestlelab.router;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Router extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Hello Servlet!");
+
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+        // TODO
+    }
+}
