e67c41195da02a07e69333be80da20dad3ead6f2
cynricshu <cynricshu@gmail.com>
2014-03-31 14:11:46 +0800

    reorganize file structure

--- server/src/main/java/com/nestlelab/router/Router.java
+++ server/src/main/java/com/nestlelab/router/Router.java
@@ -1,5 +1,8 @@
 package com.nestlelab.router;
 
+import odata.transformer.DataTransformer;
+import odata.transformer.HqlWhereClauseTransformer;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,7 +19,14 @@ public class Router extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
+
+        String filterString = "Name eq 'a'";
+        DataTransformer transformer = new HqlWhereClauseTransformer();
+        String whereClause = transformer.transform(filterString, null);
+
         out.println("Hello Servlet!");
+        out.println(whereClause);
 
     }
 
